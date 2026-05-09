d6f28ab6943f99969d075a22a6d5d84adbf72c28
chinnasamy <chinnasamyulaganathan31@gmail.com>
2026-05-09 11:31:57 +0530

    deploy m

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: [
-    "https://auction-eight-umber.vercel.app/"
+    "https://auction-eight-umber.vercel.app"
     
   ],
   credentials: true,
