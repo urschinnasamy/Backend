00695ebf0904fca15054af6957ff7f89bd07c46b
chinnasamy <chinnasamyulaganathan31@gmail.com>
2026-05-09 10:35:59 +0530

    backend

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 // SOCKET SETUP
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Your React frontend URL
+    origin: "*", // Your React frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: [
-    "http://localhost:5173"
+    "*"
     
   ],
   credentials: true,
